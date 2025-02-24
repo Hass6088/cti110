e598d75007f6fe839a49186fefee3c84f52d28b8
Hass6088 <hassc6088@student.faytechcc.edu>
2025-02-23 21:39:13 -0500

    Add files via upload
    First Submission

---
+++ P1LAB1_HassChandler.py
@@ -0,0 +1,9 @@
+# Chandler Hass
+# 23 Feb 2025
+# P1 Lab 1
+# This program prompts the user for their first and last name separately, then welcomes them to the course.
+
+first_name = input("Enter your first name:")
+last_name = input("Enter your last name:")
+
+print(f"Hello, {first_name} {last_name}! Welcome to CTI-110")
