156b6aa9b9764a03098d7f2555c90cbe89a06508
Hass6088 <hassc6088@student.faytechcc.edu>
2025-03-06 20:28:23 -0500

    Add files via upload

---
+++ P1HW1_HassChandler.py
@@ -0,0 +1,23 @@
+#Chandler Hass
+#06 March 2025
+#P1HW! 
+# This project is on using user input to do math projects
+print('-----Calculating Exponets----')
+print()
+print()
+base_value = int(input('Enter an integer as the base value: '))
+exponent = int(input('Enter an integer as the exponent: '))
+print()
+print()
+print(base_value, 'raised to the power of', exponent, 'is: ',base_value**exponent)
+print()
+print()
+print('-----Addition and Subttraction----')
+print()
+print()
+starting_integer = int(input('Enter a starting integer: '))
+add = int(input('Enter an integer to add: '))
+subtract = int(input('Enter an integer to subtract: '))
+print()
+print()
+print(starting_integer, '+', add, '-', subtract, 'is equal to ', starting_integer+add-subtract)
